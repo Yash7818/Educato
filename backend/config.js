c1dff601780ccbbcde11dc2df07124e4fc25510c
unknown <yashwandhare1234@gmail.com>
2020-09-29 19:36:57 +0530

    setted up mongo atlas:temporary

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1/video-chat",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://yash7818-taskapp:n8tivaLmnayE8T4f@cluster0.fcrzd.mongodb.net/video-chat?retryWrites=true&w=majority",
   JWT_SECRET: process.env.JWT_SECRET || "thisisit",
 };
